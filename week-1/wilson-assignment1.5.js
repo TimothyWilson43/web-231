2ac71ad6c5f967ecee62fb608a434654143d75a6
Timothy Wilson <wilsonxchevy@gmail.com>
2018-05-26 21:50:08 -0500

    Adding wilson-assignment1.5.js

---
+++ week-1/wilson-assignment1.5.js
@@ -0,0 +1,89 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Timothy Wilson
+; Date:   24 May 2018
+; Modified By: Timothy Wilson
+; Description: This program demonstrates the
+;   use of JavaScript types, values, and 
+;   and variables in an application.
+;===========================================
+*/ 
+
+
+var myFirstName = "Timothy";
+var myLastName = "Wilson";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+
+
+var firstName = "Timothy";
+var lastName = "Wilson";
+var middleName = "Dave";
+var homeAddress = "100 Fake Street";
+var rateOfPay = "$12/hr";
+var hireDate = "1 June 2000";
+
+var employeeOne = "\n" + firstName + " " + middleName + " " + lastName + "\n" + homeAddress + 
+    "\nRate of Pay: " + rateOfPay + "\nDate of Hire: " + hireDate;
+
+var firstName = "Joe";
+var lastName = "Blow";
+var middleName = "Q.";
+var homeAddress = "103 Fake Drive";
+var rateOfPay = "$12.50/hr";
+var hireDate = "2 June 2000";
+    
+var employeeTwo = "\n" + firstName + " " + middleName + " " + lastName + "\n" + homeAddress + 
+    "\nRate of Pay: " + rateOfPay + "\nDate of Hire: " + hireDate;
+
+var firstName = "Jane";
+var lastName = "Doe";
+var middleName = "Louise";
+var homeAddress = "105 Fake Circle";
+var rateOfPay = "$13/hr";
+var hireDate = "3 June 2000";
+        
+var employeeThree = "\n" + firstName + " " + middleName + " " + lastName + "\n" + homeAddress + 
+    "\nRate of Pay: " + rateOfPay + "\nDate of Hire: " + hireDate;
+    
+var firstName = "Harry";
+var lastName = "Hendersons";
+var middleName = "and the";
+var homeAddress = "107 Fake Lane";
+var rateOfPay = "$15/hr";
+var hireDate = "4 June 2000";
+            
+var employeeFour = "\n" + firstName + " " + middleName + " " + lastName + "\n" + homeAddress + 
+   "\nRate of Pay: " + rateOfPay + "\nDate of Hire: " + hireDate;
+
+var firstName = "Java";
+var lastName = "Script";
+var middleName = "S.";
+var homeAddress = "109 Fake Road SE";
+var rateOfPay = "$28/hr";
+var hireDate = "5 June 2000";
+               
+var employeeFive = "\n" + firstName + " " + middleName + " " + lastName + "\n" + homeAddress + 
+  "\nRate of Pay: " + rateOfPay + "\nDate of Hire: " + hireDate;
+    
+
+console.log(employeeOne);
+console.log(employeeTwo);
+console.log(employeeThree);
+console.log(employeeFour);
+console.log(employeeFive);
+
+// end of program 
+
+
+
+
