be2f9ba0aa25e187a676eb2d2da3d2ff6073b694
Timothy Wilson <wilsonxchevy@gmail.com>
2018-05-29 20:58:40 -0500

    Adding assignment 2.2

---
+++ week-2/wilson-exercise-2.2.js
@@ -0,0 +1,6 @@
+var header = require('../header');
+
+console.log("\nTesting the header.js file's output...");
+console.log(header.display("Timothy", "Wilson", "wilson-exercise-2.2"));
+console.log("\n");
+
