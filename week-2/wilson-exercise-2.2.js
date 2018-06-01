1b27e124cfa16a6da6c0ba58bce326a3ef9464d8
Timothy Wilson <tiwilson@my.bellevue.edu>
2018-05-31 22:00:21 -0500

    Adding assignment 2.2

--- week-2/wilson-exercise-2.2.js
+++ week-2/wilson-exercise-2.2.js
@@ -1,3 +1,12 @@
+/* ============================================ 
+; Title: Assignment 2.2
+; Author: Timothy Wilson
+; Date: 30 May 2018
+; Modified By: Timothy Wilson
+; Description: This program demonstrates the 
+; use of attaching a header to an assignment.
+;=========================================== */
+
 var header = require('../header');
 
 console.log("\nTesting the header.js file's output...");
